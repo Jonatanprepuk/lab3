3e50d2b0a60897a3f9a5a76a5262971df1110452
jonatanprepuk <jonatanprepuk@gmail.com>
2026-09-18 11:33:16 +0200

    fixed formating in tests

--- tests/testthat/test_euclidean.R
+++ tests/testthat/test_euclidean.R
@@ -8,7 +8,7 @@ test_that("GDC is calculated correctly.", {
 
 
 test_that("Wrong input throws an error.", {
-  expect_error(euclidean("100", 1000))  
+  expect_error(euclidean("100", 1000))
   expect_error(euclidean(100, "1000"))
-  expect_error(euclidean(TRUE, "1000"))  
+  expect_error(euclidean(TRUE, "1000"))
 })
