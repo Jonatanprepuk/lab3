716bfe0a18710ba96a7309746364ecb3981b36e0
Albin Bäckström <albinbackstrom@macbookpro.students.netlogon.liu.se>
2026-09-18 10:15:19 +0200

    Euclidean function with roxygen2 comments

---
+++ euclidean.r
@@ -0,0 +1,39 @@
+#' Calculate the greatest common divisor using the Euclidean algorithm
+#'
+#' The Euclidean algorithm calculates the greatest common divisor
+#' of two numbers by repeatedly replacing the larger number with the
+#' remainder obtained when dividing the larger number by the smaller one.
+#' This process continues until the remainder is zero. The last non-zero
+#' remainder is the greatest common divisor.
+#'
+#' @param M A numeric vector of length one containing the first number.
+#' @param N A numeric vector of length one containing the second number.
+#'
+#' @return The greatest common divisor of M and N.
+#'
+#' @references
+#' \url{https://en.wikipedia.org/wiki/Euclidean_algorithm}
+#'
+#' @examples
+#' euclidean(123612, 13892347912)
+#' euclidean(100, 1000)
+#'
+
+euclidean <- function(M, N){
+  stopifnot(is.vector(c(M, N)))  #  Stop if either parameter is not a vector
+  if (M < N){    #  Swap M and N if M is smaller
+    m = N
+    n = M 
+  } else {
+    m = M
+    n = N
+  }
+  R = NA
+  while (n != 0 ) {
+    R = m %% n 
+    m = n
+    n = R 
+  }
+  return(m)
+}
+
