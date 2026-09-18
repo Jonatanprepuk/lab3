5c849d99221d4442bcc2f00cc3d3ee048e5a12d0
Albin Bäckström <albinbackstrom@macbookpro.students.netlogon.liu.se>
2026-09-18 11:01:44 +0200

    Updated parameter definition and stopifnot()

--- R/euclidean.r
+++ R/euclidean.r
@@ -6,8 +6,8 @@
 #' This process continues until the remainder is zero. The last non-zero
 #' remainder is the greatest common divisor.
 #'
-#' @param M A numeric vector of length one containing the first number.
-#' @param N A numeric vector of length one containing the second number.
+#' @param M A numeric value of length one containing the first number.
+#' @param N A numeric value of length one containing the second number.
 #'
 #' @return The greatest common divisor of M and N.
 #'
@@ -17,9 +17,10 @@
 #' @examples
 #' euclidean(123612, 13892347912)
 #' euclidean(100, 1000)
+#'@export
 #'
 euclidean <- function(M, N) {
-  stopifnot(is.vector(c(M, N))) #  Stop if either parameter is not a vector
+  stopifnot(is.numeric(c(M, N))) #  Stop if either parameter is not numeric
   if (M < N) { #  Swap M and N if M is smaller
     m <- N
     n <- M
