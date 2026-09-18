dbdba3869077c8f61371b74832796c47790bc01f
jonatanprepuk <jonatanprepuk@gmail.com>
2026-09-18 13:18:19 +0200

    fixed documentation for wiki_data

---
+++ R/data.R
@@ -0,0 +1,10 @@
+#' A graph dataset with 6 vertecies and 9 edges.
+#' @format ## `wiki_graph`
+#' A data frame with 18 rows and 3 columns:
+#' \describe{
+#'   \item{v1}{Vertex 1}
+#'   \item{v2}{Vertex 2}
+#'   \item{w}{weight}
+#' }
+#' @source <https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm>
+"wiki_graph"
