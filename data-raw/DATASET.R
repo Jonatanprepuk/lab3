a87c2db3877a460a835fa989960e0ea9d6e35923
jonatanprepuk <jonatanprepuk@gmail.com>
2026-09-18 12:49:29 +0200

    update documentation in dataset

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,10 +1,10 @@
 ## code to prepare `DATASET` dataset goes here
-#' Example graph to test dijkstra's algorithm
+#' A graph dataset with 6 vertecies and 9 edges.
 #' @format ## `wiki_graph`
 #' A data frame with 18 rows and 3 columns:
 #' \describe{
-#'   \item{v1}{Start vertex}
-#'   \item{v2}{End vertex}
+#'   \item{v1}{Vertex 1}
+#'   \item{v2}{Vertex 2}
 #'   \item{w}{weight}
 #' }
 #' @source <https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm>
