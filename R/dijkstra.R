d1aa12d0235a89f7f09846ab1fd35f0b730e94f8
jonatanprepuk <jonatanprepuk@gmail.com>
2026-09-21 12:46:05 +0200

    documentation added for dijkstra

--- R/dijkstra.R
+++ R/dijkstra.R
@@ -1,3 +1,34 @@
+#' Calculate shortest path distances using Dijkstra's algorithm
+#'
+#' Dijkstra's algorithm finds the shortest path distances from one
+#' initial node to all other nodes in a directed weighted graph.
+#'
+#' @param graph A \code{\link[base]{data.frame}} representing the graph.
+#' Each row represent an edge and the data fram must contain the following
+#' columns:
+#' \describe{
+#'    \item{\code{v1}}{A \code{vector} containing start nodes}
+#'    \item{\code{v2}}{A \code{vector} containing destination nodes}
+#'    \item{\code{w}}{A \code{vector} containing the weights from v1_i to v2_i}
+#' }
+#' All edge weights must be non-negative.
+#'
+#' @param init_node An integer specifying the initial node from which
+#' shortest path distances are calculated. The node must occur in either
+#' \code{v1} or \code{v2}.
+#'
+#' @return A numeric \code{link[base]{vector}} containing the shortest
+#'  path distance from \code{init_node} to all other nodes.
+#'
+#' @references
+#' \url{https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm}
+#'
+#' @examples
+#' data(wiki_graph)
+#' dijkstra(wiki_graph, 1)
+#' dijkstra(wiki_graph, 6)
+#' @export
+#'
 dijkstra <- function(graph, init_node) {
   stopifnot(
     is.data.frame(graph),
